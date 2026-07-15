19fa2c177e761f493f66fecb141a7c77ddc9c512
Marco <tirinatomarco@gmail.com>
2026-07-15 22:16:57 +0200

    flusso mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Indirizzo pubblico del sito
-  site: 'https://italiannextgen.it',
+  // Indirizzo pubblico del sito (passerà a https://italiannextgen.it quando i DNS Aruba saranno attivi)
+  site: 'https://tiri6.github.io',
 });
